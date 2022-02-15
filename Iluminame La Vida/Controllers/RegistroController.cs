a843e6b8b42a9bb51e64dc8c4ffdb3f080294c2c
RAFAEL CAUICH COLLI <20090447@alumno.utmetropolitana.edu.mx>
2022-02-14 20:12:57 -0600

    A ver si ahora si

--- Iluminame La Vida/Controllers/RegistroController.cs
+++ Iluminame La Vida/Controllers/RegistroController.cs
@@ -62,6 +62,56 @@ namespace Iluminame_La_Vida.Controllers
             }
             return Ok(oRespuesta);
         }
+        [HttpPut]
+        //Este metodo sirve para editar los correos
+
+        public IActionResult Edit(RegistroRequest model)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (IluminameLaVidaContext db = new IluminameLaVidaContext())
+                {
+                    Registro oPro = db.Registros.Find(model.Id_Usuario);
+                    oPro.Mail = model.mail;
+                    oPro.Pass = model.pass;
+                    oPro.Nombre = model.nombre;
+                    oPro.Apellidos = model.apellidos;
+                    oPro.Colonia = model.colonia;
+                    db.Entry(oPro).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+        }
+
+        [HttpDelete("{Id}")]
+        //Con este metodo vamos a eliminar cualquiera que querramos
+        public IActionResult Del(int Id)
+        {
+            Respuesta oRespuesta = new Respuesta();
+
+            try
+            {
+                using (IluminameLaVidaContext db = new IluminameLaVidaContext())
+                {
+                    Registro oPro = db.Registros.Find(Id);
+                    db.Remove(oPro);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+        }
 
         /* se puede usar el siguiente json para probar el metodo post
         {
--- Iluminame La Vida/Controllers/ReporteController.cs
+++ Iluminame La Vida/Controllers/ReporteController.cs
@@ -69,7 +69,61 @@ namespace Iluminame_La_Vida.Controllers
             }
             return Ok(oRespuesta);
         }
-        
+
+        [HttpPut]
+
+        //Este metodo sirve para editar los datos
+
+        public IActionResult Edit(ReporteRequest model)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            try
+            {
+                using (IluminameLaVidaContext db = new IluminameLaVidaContext())
+                {
+                    Reporte oPro = db.Reportes.Find(model.Id_Reporte);
+                    oPro.Descrip = model.descrip;
+                    oPro.Foto = model.foto;
+                    oPro.Etiquetas = model.etiquetas;
+                    oPro.Colonia = model.colonia;
+                    oPro.IdUsuario = model.Id_Usuario;
+                    oPro.Coordenadas = model.coordenadas;
+                    db.Entry(oPro).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+        }
+
+        [HttpDelete("{Id}")]
+        //Con este metodo vamos a eliminar cualquiera que querramos
+        public IActionResult Del(int Id)
+        {
+            Respuesta oRespuesta = new Respuesta();
+
+            try
+            {
+                using (IluminameLaVidaContext db = new IluminameLaVidaContext())
+                {
+                    Reporte oPro = db.Reportes.Find(Id);
+                    db.Remove(oPro);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+        }
+
+
         /* se puede usar el siguiente json para probar el metodo post
         {
             "descrip": "ta feo",
